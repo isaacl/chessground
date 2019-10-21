a70a5d69eb8f5e62eb6418419991f701251b4e3b
Isaac Levy <isaac.r.levy@gmail.com>
2019-10-21 18:06:09 -0400

    m

--- src/dragHtml.ts
+++ src/dragHtml.ts
@@ -8,23 +8,18 @@ const lichessKey = 'application/lichess.origin'
 
 export const onDragStart = (s: State) => (e: DragEvent) => {
   if (!e.target || !e.dataTransfer) return;
-  const t = e.target as HTMLElement,
-  // I'd prefer e.clientX but that seems to be the location the drag starts,
-  // e.clientX doesn't always refer to the right square but maybe there's
-  // a workaround.
-  rect = t.getBoundingClientRect(), // TODO map (elem) -> js obj for faster lookup of coord
-  position = [rect.left + rect.width / 2, rect.top + rect.height / 2] as cg.NumberPair,
-  orig = board.getKeyAtDomPos(position, board.whitePov(s), s.dom.bounds());
+  const t = e.target as cg.PieceNode,
+  orig = t.cgKey;
   console.log("ondragstart", orig);
 
   if (s.drawable.enabled && s.drawable.eraseOnClick) // TODO || piece.color !== s.turnColor
     drawClear(s);
 
-  e.dataTransfer.setData(lichessKey, orig!.toString());
+  e.dataTransfer.setData(lichessKey, orig.toString());
   e.dataTransfer.effectAllowed = 'move';
   // (event.dataTransfer as any).mozCursor = 'auto';
   // sigh.
-  const offset = rect.width / (navigator.userAgent.search("Firefox") > 0 ? window.devicePixelRatio : 1);
+  const offset = t.clientWidth / (navigator.userAgent.search("Firefox") > 0 ? window.devicePixelRatio : 1);
   // The grabber cursor blocks more of the piece so offset it to grab the piece lower
   e.dataTransfer.setDragImage(t, offset, offset * 1.3);
 
